9d42f6bb3f76043a19cb46bce91695ec7b4f117e
Ozge Islegen <ozgeislegen@Ozges-MacBook-Pro.local>
2019-06-03 17:40:10 -0700

    reorganized files

--- 1-Charity-ML_Project-XGBoost-RF-LogisticRegression-Python/visuals.py
+++ 1-Charity-ML_Project-XGBoost-RF-LogisticRegression-Python/visuals.py
@@ -71,11 +71,11 @@ def evaluate(results, accuracy, f1):
         for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
             for i in np.arange(3):
                 
-                # Creative plot code
+                # Create plot code
                 ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
                 ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
                 ax[j//3, j%3].set_xticklabels(["1%", "10%", "100%"])
-                ax[j//3, j%3].set_xlabel("Training Set Size")
+              #  ax[j//3, j%3].set_xlabel("Training Set Size")
                 ax[j//3, j%3].set_xlim((-0.1, 3.0))
     
     # Add unique y-labels
@@ -87,12 +87,12 @@ def evaluate(results, accuracy, f1):
     ax[1, 2].set_ylabel("F-score")
     
     # Add titles
-    ax[0, 0].set_title("Model Training")
-    ax[0, 1].set_title("Accuracy Score on Training Subset")
-    ax[0, 2].set_title("F-score on Training Subset")
-    ax[1, 0].set_title("Model Predicting")
-    ax[1, 1].set_title("Accuracy Score on Testing Set")
-    ax[1, 2].set_title("F-score on Testing Set")
+    #ax[0, 0].set_title("Model Training")
+    #ax[0, 1].set_title("Accuracy Score on Training Subset")
+    #ax[0, 2].set_title("F-score on Training Subset")
+    #ax[1, 0].set_title("Model Predicting")
+    #ax[1, 1].set_title("Accuracy Score on Testing Set")
+    #ax[1, 2].set_title("F-score on Testing Set")
     
     # Add horizontal lines for naive predictors
     ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
